18ba3d8596cc28074add3f25dc3bbab0b962351d
ADMIN <vladcsw@gmail.com>
2023-02-22 23:13:27 -0500

    First

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+function App() {
+  return (
+    <div>
+      <header>
+        <a href="/"> amazona </a>
+        {/* <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a> */}
+      </header>
+      <main>list produ ctos</main>
+    </div>
+  );
+}
+
+export default App;
